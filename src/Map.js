c7f592be41889225b93c03a5918cad5ffaa36c48
Dhruv Batra <dhruvbatra2@gmail.com>
2021-05-22 05:41:24 -0400

    checkpoint

--- src/Map.js
+++ src/Map.js
@@ -1,23 +1,6 @@
 import BingMapsReact from "bingmaps-react";
+import { ReactBingmaps } from 'react-bingmaps-plus';
 const MAP_KEY = process.env.REACT_APP_MAP_ACCESS_TOKEN;
-
-var map = new BingMapsReact.Map('#myMap', {
-    credentials: {MAP_KEY},
-    center: new BingMapsReact.Location(47.6149, -122.1941)
-});
-
-var center = map.getCenter();
-
-//Create custom Pushpin
-var pin = new BingMapsReact.Pushpin(center, {
-    title: 'Microsoft',
-    subTitle: 'City Center',
-    text: '1'
-});
-
-//Add the pushpin to the map
-map.entities.push(pin);
-
 
 export default function Maps(){
 
@@ -33,18 +16,7 @@ export default function Maps(){
                 viewOptions={{
                     center: { latitude: 42.360081, longitude: -71.058884 },
                 }}
-                pushPins = {
-                    [
-                      {
-                        title: 'Microsoft',
-                        subTitle: 'City Center',
-                        text: '1',
-                        location:'[42.360081, -71.058884]'
-                      }
-                    ]
-                  }
             />
-            <div id="myMap" style="position:relative;width:600px;height:400px;"></div>
         </div>
     )
 }
