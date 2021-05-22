f26e37a9352d98771d4f7b0e96b67a33de2809fc
Dhruv Batra <dhruvbatra2@gmail.com>
2021-05-22 03:05:59 -0400

    images

--- src/Item.js
+++ src/Item.js
@@ -1,70 +1,91 @@
-import { useEffect } from "react"
-import { DataGrid, GridColDef } from '@material-ui/data-grid';
 import { Button } from "@material-ui/core";
+import { makeStyles } from '@material-ui/core/styles';
+import Grid from '@material-ui/core/Grid';
+import Paper from '@material-ui/core/Paper';
+import Typography from '@material-ui/core/Typography';
+import ButtonBase from '@material-ui/core/ButtonBase';
 
 const API_KEY = process.env.REACT_APP_api_key;
 
-export default function Item({itemList}){
+const useStyles = makeStyles((theme) => ({
+    root: {
+      flexGrow: 1,
+    },
+    paper: {
+      padding: theme.spacing(4),
+      margin: 'auto',
+      maxWidth: 500,
+    },
+    image: {
+      width: 128,
+      height: 128,
+    },
+    img: {
+      margin: 'auto',
+      display: 'block',
+      maxWidth: '100%',
+      maxHeight: '100%',
+    },
+  }));
 
-    function onClick(e) {
-        console.log("hi")
+export default function Item({itemList}){
+    const classes = useStyles();
+
+    function getImage(photoId) {
+      const url = new URL("https://maps.googleapis.com/maps/api/place/photo");
+      url.searchParams.append("key", API_KEY);
+      url.searchParams.append("maxheight", "128");
+      url.searchParams.append("maxwidth", "128");
+      url.searchParams.append("photoreference", photoId);
+      console.log(url);
+      return(<img src={url['href']}></img>)
     }
-    
-    const columns: GridColDef[] = [
-        { field: 'firstName', headerName: 'First name', width: 150 },
-        { field: 'lastName', headerName: 'Last name', width: 150 },
-        {
-          field: 'age',
-          headerName: 'Age',
-          type: 'number',
-          width: 110,
-        },
-        {
-          field: 'website',
-          headerName: 'Website',
-          sortable: false,
-          width: 160,
-          renderCell: (params: rows) => {
-            return( 
-                <Button
-                    variant="contained"
-                    color="primary"
-                    href="params.getValue("
-                >Get Directions</Button>);
-          }
-        },
-        {
-            field: 'dir',
-            headerName: 'Directions',
-            description: 'This column has a value getter and is not sortable.',
-            sortable: false,
-            width: 160,
-            renderCell: (params) => {
-              return( 
-                  <Button
-                      variant="contained"
-                      color="primary"
-                  >Get Directions</Button>);
-            }
-          },
-      ];
-      
-      const rows = [
-        { id: 1, lastName: 'Snow', firstName: 'Jon', age: 35, website:'https://google.com'},
-        { id: 2, lastName: 'Lannister', firstName: 'Cersei', age: 42 },
-        { id: 3, lastName: 'Lannister', firstName: 'Jaime', age: 45 },
-        { id: 4, lastName: 'Stark', firstName: 'Arya', age: 16 },
-        { id: 5, lastName: 'Targaryen', firstName: 'Daenerys', age: null },
-        { id: 6, lastName: 'Melisandre', firstName: null, age: 150 },
-        { id: 7, lastName: 'Clifford', firstName: 'Ferrara', age: 44 },
-        { id: 8, lastName: 'Frances', firstName: 'Rossini', age: 36 },
-        { id: 9, lastName: 'Roxie', firstName: 'Harvey', age: 65 },
-      ];
+    //{getImage(item['photos'][0]['photo_reference'])}
+
+    //console.log(itemList[0]['photos'][0]['photo_reference']);
 
     return(
         <div>
-            <div style={{ height: 400, width: '100%' }}>
-                <DataGrid rows={rows} columns={columns} pageSize={5}/>
+            <div>
+                {itemList.map((item) => (
+                    <div className={classes.root}>
+                    <Paper className={classes.paper}>
+                      <Grid container spacing={2}>
+                        <Grid item>
+                          <ButtonBase className={classes.image}>
+                            {getImage(item['photos'][0]['photo_reference'])}
+                          </ButtonBase>
+                        </Grid>
+                        <Grid item xs={12} sm container>
+                          <Grid item xs container direction="column" spacing={2}>
+                            <Grid item xs>
+                              <Typography gutterBottom variant="h5">
+                                {item['name']}
+                              </Typography>
+                              <Typography variant="body1" gutterBottom>
+                              { (item['price_level'] != undefined) ? "Rating: "+item['rating']+"/5" : ''}
+                              </Typography>
+                              <Typography variant="body1" color="textSecondary">
+                              { (item['price_level'] != undefined) ? "Price: "+item['price_level']+"/5" : ''}
+                              </Typography>
+                              <Typography variant="body1" color="textSecondary">
+                              { (item['vicinity'] != undefined) ? "Vicinity: "+item['vicinity'] : ''}
+                              </Typography>
+                            </Grid>
+                            <Grid item>
+                              <Typography variant="body2" style={{ cursor: 'pointer' }}>
+                                Remove
+                              </Typography>
+                            </Grid>
+                          </Grid>
+                          <Grid item>
+                            <Typography variant="subtitle1">$19.00</Typography>
+                          </Grid>
+                        </Grid>
+                      </Grid>
+                    </Paper>
+                  </div>
+                ))}
             </div>
         </div>
     )
--- src/Searcher.js
+++ src/Searcher.js
@@ -5,14 +5,14 @@ const API_KEY = process.env.REACT_APP_api_key;
 
 export default function Searcher(){
 
-    const [data, setData] = useState({'results':[{'website':["Failed"]}]});
+    const [data, setData] = useState({'results':[{'photos':[{'photoreference':["Failed"]}]}]});
 
     useEffect(() => {
         const url = new URL("https://maps.googleapis.com/maps/api/place/nearbysearch/json");
         url.searchParams.append("location", "-33.8670522,151.1957362");
         url.searchParams.append("radius", "1500");
         url.searchParams.append("keyword", "restaurant");
-        url.searchParams.append("fields", "formatted_address,name,rating,price_level,opennow,website");
+        url.searchParams.append("fields", "formatted_address,name,rating,price_level,opennow,website,vicinity");
         url.searchParams.append("key", API_KEY);
         fetch(url)
         .then((resp) => {
