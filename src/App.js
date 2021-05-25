2c2bb5d8fb741e3e5b6bea51772c49b9d96ac15e
Dhruv Batra <dhruvbatra2@gmail.com>
2021-05-24 22:46:54 -0400

    navbar

--- src/App.js
+++ src/App.js
@@ -3,17 +3,20 @@ import './App.css';
 import Main from './components/Main'
 import React, { Component } from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Navbar from './components/navbar'
+import { Button } from "@material-ui/core";
 function App() {
 
   document.title = "Find Dining";
 
   return (
     <div className="App">
-      <header className="App-header">
-        <h2>Find Dining</h2>
-      </header>
       <div className="App-body">
         <br></br>
+        <div>
+        <Navbar/>
+        <br></br>
+      </div>
         <Switch>
           <Route path='/' component={Main} />
         </Switch>
--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,9 +1,19 @@
+import React, { Component } from 'react';
+import { Button } from "@material-ui/core";
+import { BrowserRouter, Route, Switch, Link } from 'react-router-dom';
+
 export default function Navbar() {
     return (
       <div>
-        <Link to="/">Home </Link>
-        <Link to="/about">About Us </Link>
-        <Link to="/shop">Shop Now </Link>
+        <header className="App-header">
+          <h2>Find Dining</h2>
+          <div class="navbaar">
+            <Button to="/" variant="contained">Home</Button>
+            <Button href="/weather" variant="contained">Check the Weather</Button>
+          </div>
+          <br></br>
+        </header>
+        <br></br>
       </div>
     );
   };
